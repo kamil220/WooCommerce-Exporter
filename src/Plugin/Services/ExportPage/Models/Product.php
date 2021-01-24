991fc47b69766951c6bfcd5d38032294a03d1fc7
Kamil Łazarz <contact@webwolf.dev>
2021-01-24 20:47:18 +0100

    Update product model

--- src/Plugin/Services/ExportPage/Models/Product.php
+++ src/Plugin/Services/ExportPage/Models/Product.php
@@ -7,6 +7,8 @@
 
 namespace WPDesk\WoocommerceExporter\Services\ExportPage\Models;
 
+use WC_Product;
+
 /**
  * WooCommerce Exporter Product model
  *
@@ -38,14 +40,29 @@ class Product {
 	/**
 	 * Product price after discount
 	 *
-	 * @var float $price
+	 * @var string $price
 	 */
 	public $price;
 
 	/**
 	 * Regular product price
 	 *
-	 * @var float $regular_price
+	 * @var string $regular_price
 	 */
 	public $regular_price;
+
+	/**
+	 * Product constructor
+	 *
+	 * @param WC_Product $product WooCommerce product model.
+	 */
+	public function __construct( WC_Product $product ) {
+
+		$this->name = $product->get_name();
+		$this->regular_price = $product->get_regular_price();
+		$this->price = $product->get_price();
+		$this->sku = $product->get_sku();
+		$this->categories = $product->get_category_ids();
+
+	}
 }
