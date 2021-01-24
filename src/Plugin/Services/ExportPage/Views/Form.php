1cc178fe7026c3084639f0f81e534d0bb9fd54e9
Kamil Łazarz <contact@webwolf.dev>
2021-01-24 20:33:45 +0100

    Add export form

--- src/Plugin/Services/ExportPage/Views/Form.php
+++ src/Plugin/Services/ExportPage/Views/Form.php
@@ -12,6 +12,25 @@ if ( ! \defined( 'ABSPATH' ) ) {
 
 ?>
 
-<form class="woocommerce-exporter-form">
+<form class="woocommerce-exporter-form" method="post" action="#">
+	<?php
+		/**
+		 * Add WordPress nonce for security reason
+		 */
+		\wp_nonce_field( 'woocommerce-exporter_export-form' );
 
+		/**
+		 * Add hook which allows to extend the form fields
+		 *
+		 * @hook wpdesk_woocommerce_exporter_form_fields
+		 * @notice Personally I prefer following notation WPDesk\WoocommerceExporter\Services\ExportPage\Form\Fields
+		 */
+		\do_action( 'wpdesk_woocommerce_exporter_form_fields' );
+
+
+		/**
+		 * Add WordPress submit button
+		 */
+		\submit_button( __( 'Export to CSV', 'woocommerce-exporter' ) );
+	?>
 </form>
