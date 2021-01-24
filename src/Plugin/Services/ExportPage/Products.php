0de53bc66980a072aa5e5216c3802471a706d71c
Kamil Łazarz <contact@webwolf.dev>
2021-01-25 00:58:53 +0100

    PHPCS fixes

--- src/Plugin/Services/ExportPage/Products.php
+++ src/Plugin/Services/ExportPage/Products.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Products controller
+ *
+ * @package WPDesk\WoocommerceExporter
  */
 
 namespace WPDesk\WoocommerceExporter\Services\ExportPage;
@@ -13,23 +15,25 @@ use WPDesk\WoocommerceExporter\Services\ExportPage\Models\Product;
  * @package WPDesk\WoocommerceExporter\Services\ExportPage
  * @author Kamil Łazarz
  */
-class Products
-{
+class Products {
 	/**
 	 * Get product for export
 	 *
-	 * @param array $args post query args
+	 * @todo add chunks
+	 * @param array $args post query args.
 	 * @return array
 	 */
 	public function get_products( $args = [] ) {
 
-		$args = shortcode_atts( [
-			'post_type' => 'product',
-			'posts_per_page' => -1,
-			'orderby'          => 'date',
-			'order'            => 'DESC',
-			'post_status'	=> 'any'
-		], $args );
+		$args = shortcode_atts(
+			[
+				'post_type'      => 'product',
+				'posts_per_page' => 100,
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+				'post_status'    => 'any',
+			], $args
+		);
 
 		return $this->get_products_models( $args );
 	}
@@ -37,26 +41,26 @@ class Products
 	/**
 	 * Get woocommerce product and prepare data
 	 *
-	 * @param $args
+	 * @param array $args get posts arguments.
 	 * @return array
 	 */
 	private function get_products_models( $args ) {
 
 		$woo_products = get_posts( $args );
-		$products = [];
+		$products     = [];
 
-		if( empty( $woo_products ) ) {
+		if ( empty( $woo_products ) ) {
 			return [];
 		}
 
 		foreach ( $woo_products as $product ) {
-			$product = new \WC_Product_Variable( $product );
+			$product    = new \WC_Product_Variable( $product );
 			$variations = $product->get_children();
 			$products[] = new Product( $product );
 
-			if( !empty( $variations ) ){
+			if ( ! empty( $variations ) ) {
 				foreach ( $variations as $variation ) {
-					$variation = new \WC_Product_Variation( $variation );
+					$variation  = new \WC_Product_Variation( $variation );
 					$products[] = new Product( $variation, $product );
 				}
 			}
