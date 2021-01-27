da0f7b9e62cc4b0c213401f038ff63440b70335b
Kamil Łazarz <contact@webwolf.dev>
2021-01-27 18:56:12 +0100

    Add nonce, a adjust submit button

--- src/Plugin/Services/ExportPage/Views/Form.php
+++ src/Plugin/Services/ExportPage/Views/Form.php
@@ -12,12 +12,12 @@ if ( ! \defined( 'ABSPATH' ) ) {
 
 ?>
 
-<form class="woocommerce-exporter-form" method="post" action="#">
+<form id="woocommerce-exporter-form" class="woocommerce-exporter-form" method="post" action="#">
 	<?php
 		/**
 		 * Add WordPress nonce for security reason
 		 */
-		\wp_nonce_field( 'woocommerce-exporter_export-form' );
+		\wp_nonce_field( 'woocommerce-exporter_export-form', 'woocommerce-exporter_export-form' );
 
 		/**
 		 * Add hook which allows to extend the form fields
@@ -31,6 +31,11 @@ if ( ! \defined( 'ABSPATH' ) ) {
 		/**
 		 * Add WordPress submit button
 		 */
-		\submit_button( __( 'Export to CSV', 'woocommerce-exporter' ) );
-	?>
+		\submit_button( __( 'Regenerate CSV', 'woocommerce-exporter' ), 'primary', 'submit', false,
+			[
+				'data-default-text' => __( 'Regenerate CSV', 'woocommerce-exporter' ),
+				'data-loading-text' => __( 'Loading...', 'woocommerce-exporter' ),
+			]
+		);
+		?>
 </form>
